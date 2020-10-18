73f6df650e55997a6ecc5cd8b499130f49accff7
Holoborodko Diana <d.goloborodko28@gmail.com>
2020-10-18 18:07:54 +0300

    task complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
   
-}
+   if (n < 0){
+      return n = (n * -1).toString().split('').reverse().join('');
+    }
+     else return n.toString().split('').reverse().join('');
+  }
+
